24bcfd9d80c8f2994247e7e73649485e79c92813
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-05-19 23:02:55 +0300

    выполнено 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 
 
     //testImplementation("org.junit.platform:junit-platform-launcher:1.9.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")  // этот один заменяет все три - специально сделано
+    //testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")  // этот один заменяет все три - специально сделано
 //    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
 //    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 //    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
